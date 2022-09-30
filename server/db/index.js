0ac07c9ea2095fb1bd1a19cf577196a81df6bc62
Tessa Thornberry <tthornberryclass@gmail.com>
2022-09-30 12:10:46 -0700

    add dependencies - server up and running and terminal is responsive to postman

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,15 @@
 var mysql = require('mysql2');
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  // host: "localhost",
   user: "root",
   password: "",
   database: "movielist"
 });
 
-connection.connect();
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected! db -index.js");
+});
 
 module.exports = connection;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,43 @@
-const express = require('express');
-const app = express();
-const PORT = 3000 || process.env.PORT;
+// const express = require('express');
+// const app = express();
+// const PORT = 3000 || process.env.PORT;
 
-app.use(express.static('client/dist'));
+// app.use(express.static('client/dist'));
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
-})
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port: ${PORT}`);
+// })
+
+var express = require('express');
+var db = require('./db');
+
+// Middleware
+var morgan = require('morgan');
+var cors = require('cors');
+
+// Router
+var router = require('./routes.js');
+
+var app = express();
+module.exports.app = app;
+
+// Set what we are listening on.
+app.set('port', 3000);
+
+// Logging and parsing
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// Set up our routes
+app.use('/classes', router);
+
+// Serve the client files
+app.use(express.static(__dirname + '/../client'));
+
+// If we are being run directly, run the server.
+if (!module.parent) {
+  app.listen(app.get('port'));
+  console.log('Listening on', app.get('port'));
+}
+
